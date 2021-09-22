41810fbe0b11cebe606dde6e1f52c7d7c9d608fb
Hieu <xuanhieu0012@gmail.com>
2021-09-21 23:38:20 -0500

    commit tree 87dc2b8a4661306295f544582b560e0153e940bf

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Dog
 
+    def name
+        @name
+    end
+    def name=(name)
+        @name = name
+    end
+
+    def breed
+        @breed
+    end
+    def breed=(breed)
+        @breed = breed
+    end
 end
+
+fido = Dog.new
+fido.name = ("Fido")
+
+beagle = Dog.new
+beagle.breed = ("Beagle")
